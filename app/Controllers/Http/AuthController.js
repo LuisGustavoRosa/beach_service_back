61f574b2ef5ce171a1fe4d6bdb0e4c6ce8e33c2f
Luis Gustavo Rosa Cardoso <luis.cardoso@rede.ulbra.br>
2021-04-29 21:22:20 -0300

    Iniciando sql na tabela produtos e categorias
    Criar o sql para listar os produtos e suas categorias

--- app/Controllers/Http/AuthController.js
+++ app/Controllers/Http/AuthController.js
@@ -2,26 +2,50 @@
 
 const User = use("App/Models/User");
 
+const {validateAll} = use("Validator");
+
+
 class AuthController {
-  async register({ request }) {
+  async register({ request, response }) {
+
+    const rules = {
+      username:'required',
+      email:'required',
+      password:'required',
+      cep:'required',
+      telefone:'required',
+      data_nascimento:'required',
+      tipo_user:'required'
+    }
+
+    const  messages ={
+        "username.required":'O nome deve ser informado',
+        "email.required":' O email deve ser informado',
+        "password.required": 'A senha deve ser informada',
+        "cep.required":" O CEP deve ser informado",
+        "telefone.required": "O telefone deve ser informado",
+        "data_nascimento.required": "Precisa informar a data de nascimento",
+        "tipo_user":"Precisa informar o tipo de usuário"
+    }
+
+    const validate = await validateAll(request.all(), rules, messages);
+
+    if(validate.fails()){
+      return response.status(401).send({message: validate.messages()})
+    }
     const data = request.only(['id','username', 'email', 'password','cep','telefone','data_nascimento','tipo_user']);
-
     const user = await User.create(data);
-
     return user;
   }
 
   async authenticate({ request, auth }) {
     const { email, password } = request.all();
-
     const token = await auth.attempt(email, password);
-
     return token;
   }
  
  async index(){
    const users= await User.all();
-
    return users;
  }
 
@@ -31,21 +55,15 @@ async show({params}){
 
 async update ({params, request}){
     const user = await User.findOrFail(params.id);
-
     const dataToUpdate= request.only(['name', 'email','password','cep','telefone','data_nascimento']);
-
     user.merge(dataToUpdate);
-
     await user.save();
-
     return user;
 }
 
 async destroy({params}){
     const user = await User.findOrFail(params.id);
-
     await user.delete();
-
     return {
         message: 'Usuário Excluido'
     }
--- start/routes.js
+++ start/routes.js
@@ -1,27 +1,12 @@
 "use strict";
 
-const AuthController = require("../app/Controllers/Http/AuthController");
-
-/*
-|--------------------------------------------------------------------------
-| Routes
-|--------------------------------------------------------------------------
-|
-| Http routes are entry points to your web application. You can create
-| routes for different URL's and bind Controller actions to them.
-|
-| A complete guide on routing is available here.
-| http://adonisjs.com/docs/4.0/routing
-|
-*/
-
 const Route = use("Route");
 
 
 //Usuário
 
 Route.post("/register", "AuthController.register");
-Route.post("/register_product", "ProductController.register_product");
+Route.post("/register_produtos", "ProductController.register_produtos");
 Route.post("/authenticate", "AuthController.authenticate");
 
 Route.group(()=>{
@@ -30,11 +15,15 @@ Route.group(()=>{
 
 
 Route.group(()=>{
-    Route.resource("products","ProductController").apiOnly();
+    Route.resource("produtos","ProductController").apiOnly();
 })
 
 //rota se chamna login usando a class UserController com o metodo login
 Route.post('/login',"UserController.login");
+
+Route
+  .post('users', 'UserController.store')
+  .validator('User')
 
 
 
