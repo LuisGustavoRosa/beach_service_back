947e93e4f2e26852513fb231888d9cbc7dcbd19c
Luis Gustavo Rosa Cardoso <luis.cardoso@rede.ulbra.br>
2021-04-29 21:32:55 -0300

    Iniciando sql na tabelaa produtos e categorias
    Criando sql para listar produtos junto com suas categorias

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,12 +1,42 @@
 'use strict'
 
 class UserController {
-   
-  async login ({request, auth}){
-      const {email, password} =request.all();
-      const {token}= await auth.attempt(email, password);
-      return token;
+  async register({ request }) {
+    const data = request.only(['id','username', 'email', 'password','cep','telefone','data_nascimento','tipo_user']);
+
+    const user = await User.create(data);
+
+    return user;
   }
+
+  async index(){
+    const users= await User.all();
+ 
+    return users;
+  }
+ 
+ async show({params}){
+     return await User.find(params.id);
+ }
+ 
+ async update ({params, request}){
+     const user = await User.findOrFail(params.id);
+     const dataToUpdate= request.only(['name', 'email','password','cep','telefone','data_nascimento']);
+     user.merge(dataToUpdate);
+     await user.save();
+     return user;
+ }
+ 
+ async destroy({params}){
+     const user = await User.findOrFail(params.id);
+     await user.delete();
+     return {
+         message: 'Usuário Excluido'
+     }
+ }
+   
+  
+  
 }
 
 module.exports = UserController;
--- start/routes.js
+++ start/routes.js
@@ -2,24 +2,22 @@
 
 const Route = use("Route");
 
-
-//Usuário
-
+Route.post('/login',"UserController.login");
 Route.post("/register", "AuthController.register");
 Route.post("/register_produtos", "ProductController.register_produtos");
 Route.post("/authenticate", "AuthController.authenticate");
 
 Route.group(()=>{
-    Route.resource("users","AuthController").apiOnly();
+    Route.resource("users","UserController").apiOnly();
 }).middleware('auth');
 
+
+// Produtos
 
 Route.group(()=>{
     Route.resource("produtos","ProductController").apiOnly();
 })
 
-//rota se chamna login usando a class UserController com o metodo login
-Route.post('/login',"UserController.login");
 
 Route
   .post('users', 'UserController.store')
