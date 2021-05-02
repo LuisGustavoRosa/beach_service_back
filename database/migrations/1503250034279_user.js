91669be5ccddd88fb9acfd653226cbfbe29c1d79
Luis Gustavo Rosa Cardoso <luis.cardoso@rede.ulbra.br>
2021-05-02 20:42:31 -0300

    mudando tabela user

--- database/migrations/1503250034279_user.js
+++ database/migrations/1503250034279_user.js
@@ -6,11 +6,11 @@ class UserSchema extends Schema {
   up () {
     this.create('users', (table) => {
       table.increments()
-      table.string('nome', 80).notNullable().unique()
+      table.string('nome', 80).notNullable()
       table.string('email', 254).notNullable().unique()
       table.string('password', 60).notNullable()
       table.string('empresa',60)
-      table.string('cep', 8).notNullable()
+      table.string('cep', 10).notNullable()
       table.string('telefone',15).notNullable()
       table.date('data_nascimento').notNullable()
       table.integer('tipo_user', 0).notNullable()
