68c1f987fc1efc9018141c2d9e8c56bef0607a4e
Luis Gustavo Rosa Cardoso <44246590+LuisGustavoRosa@users.noreply.github.com>
2022-06-30 22:37:36 -0300

    make it better

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import { createServer } from "https";
-import { Ignitor } from "@adonisjs/core/build/src/Ignitor";
+const { createServer } =  require ("https");
+const { Ignitor } = require ("@adonisjs/core/build/src/Ignitor");
 
 new Ignitor(__dirname).httpServer().start((handle) => {
   return createServer(
