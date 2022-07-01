e757efdd0e70957da4ff1a149f41bb9d4f05acd7
Luis Gustavo Rosa Cardoso <44246590+LuisGustavoRosa@users.noreply.github.com>
2022-06-30 22:35:46 -0300

    make it better

--- config/database.js
+++ config/database.js
@@ -1,36 +1,22 @@
-
-
-const fs = require('fs')
+'use strict'
 
 const Env = use('Env')
+const Helpers = use('Helpers')
+const Url = require('url-parse')
+const DATABASE_URL = new Url(Env.get('DATABASE_URL'))
 
-/**************************************************************************
-* CONFIG > DATABASE
-***************************************************************************/
+module.exports = {
 
-const config = {
- connection: Env.get('DB_CONNECTION', 'pg'),
-
- pg: {
-   client: 'pg',
-
-   connection: {
-     host: Env.get('DB_HOST', 'localhost'),
-     port: Env.get('DB_PORT', ''),
-     user: Env.get('DB_USER', 'postgres'),
-     password: Env.get('DB_PASSWORD', ''),
-     database: Env.get('DB_DATABASE', 'beach_service'),
-   },
-
-   debug: Env.get('DB_DEBUG', false),
- },
+  connection: Env.get('DB_CONNECTION', 'postgres'),
+  postgres: {
+    client: 'postgres',
+    connection: {
+      host: Env.get('DB_HOST', DATABASE_URL.hostname),
+      port: Env.get('DB_PORT', DATABASE_URL.port),
+      user: Env.get('DB_USER', DATABASE_URL.username),
+      password: Env.get('DB_PASSWORD',DATABASE_URL.password),
+      database: Env.get('DB_DATABASE', DATABASE_URL.pathname.substring(1))
+    },
+    debug: Env.get('DB_DEBUG', false)
+  }
 }
-
-// Add certificate for production environment
-if (Env.get('NODE_ENV') === 'production') {
- config.pg.connection.ssl = {
-   ca: fs.readFileSync(__dirname + '/certs/ca-database.crt'),
- }
-}
-
-module.exports = config
