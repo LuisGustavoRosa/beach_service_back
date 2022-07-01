5a1e652945f388e209428c4df6b92cd4a013299b
Luis Gustavo Rosa Cardoso <44246590+LuisGustavoRosa@users.noreply.github.com>
2022-06-30 21:53:48 -0300

    make it better

--- config/database.js
+++ config/database.js
@@ -9,11 +9,11 @@ module.exports = {
   postgres: {
     client: 'postgres',
     connection: {
-      host: Env.get('DB_HOST', ''),
+      host: Env.get('DB_HOST', 'localhost'),
       port: Env.get('DB_PORT', '542'),
-      user: Env.get('DB_USER', ''),
-      password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', '')
+      user: Env.get('DB_USER', 'postgres'),
+      password: Env.get('DB_PASSWORD', 'postgres'),
+      database: Env.get('DB_DATABASE', 'beach_service')
     },
     debug: Env.get('DB_DEBUG', false)
   }
